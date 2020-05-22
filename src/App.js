66ba3a278426b84b9f891f0abe9daffca262cca0
Divyum <divz7777@gmail.com>
2020-05-22 23:40:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,97 +1,112 @@
 import "./App.css";
-import React, { Component } from "react";
+import { Modal, Button, Form, Col } from "react-bootstrap";
+import React, { useState } from "react";
 
-const CSVToArray = (data, delimiter = ",", omitFirstRow = false) =>
-  data
-    .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
-    .split("\n")
-    .map((v) => v.split(delimiter));
+export default function App() {
+  const [show, setShow] = useState(false);
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      csv: "",
-      arr: [],
-      json: {},
-      currentPage: 1,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand">XYZ Company</a>
+      </nav>
+      <div className="modal-button">
+        <Button variant="primary" onClick={handleShow}>
+          New Employee
+        </Button>
 
-  handleChange(e) {
-    e.preventDefault();
-    var reader = new FileReader();
-    reader.onload = () =>
-      this.setState({ csv: reader.result, arr: CSVToArray(reader.result) });
-    reader.readAsText(e.target.files[0]);
-  }
-
-  handleClick = (event) => {
-    this.setState({
-      currentPage: Number(event.target.id),
-    });
-  };
-
-  render() {
-    const indexOfLastPost = this.state.currentPage * 5;
-    const indexOfFirstPost = indexOfLastPost - 5;
-    const currentPosts = this.state.arr.map((row) =>
-      row.slice(indexOfFirstPost, indexOfLastPost)
-    );
-    const pageNumbers = [];
-    if (this.state.arr.length !== 0) {
-      for (let i = 1; i <= Math.ceil(this.state.arr[0].length / 5); i++) {
-        pageNumbers.push(i);
-      }
-    }
-
-    const renderPageNumbers = pageNumbers.map((number) => {
-      return (
-        <li
-          className="waves-effect waves-light btn red accent-1"
-          style={{ marginRight: "10px" }}
-          key={number}
-          id={number}
-          onClick={this.handleClick}
-        >
-          {number}
-        </li>
-      );
-    });
-
-    return (
-      <div>
-        <nav>
-          <div className="nav-wrapper">
-            <div class="brand-logo center">CSViewer</div>
-          </div>
-        </nav>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Employee Details Form</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <div className="form-group">
+                <label for="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="Enter Name"
+                ></input>
+              </div>
+              <div className="form-group">
+                <label for="id">Employee ID</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="id"
+                  placeholder="Enter Employee ID"
+                ></input>
+              </div>
+              <div class="form-group">
+                <label for="sel1">Select Department</label>
+                <select class="form-control" id="sel1">
+                  <option>HR</option>
+                  <option>Operations</option>
+                  <option>Sales</option>
+                  <option>Marketing</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Enter Email ID"
+                ></input>
+              </div>
+              <div className="form-group">
+                <label for="doj">Date of Joining</label>
+                <input type="date" className="form-control" id="doj"></input>
+              </div>
+              <hr></hr>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </form>
+          </Modal.Body>
+        </Modal>
         <br></br>
-        <label style={{ marginLeft: "20px" }}>Choose a CSV file:</label>&nbsp;
-        <input
-          type="file"
-          accept=".csv"
-          id="file-selector"
-          onChange={this.handleChange}
-        ></input>
-        {this.state.arr.length !== 0 && (
-          <>
-            <ul style={{ marginLeft: "10px" }}>{renderPageNumbers}</ul>
-            <table>
-              {currentPosts.map((row) => {
-                return (
-                  <tr>
-                    {row.map((value) => (
-                      <td>{value}</td>
-                    ))}
-                  </tr>
-                );
-              })}
-            </table>
-          </>
-        )}
+        <h5 style={{ marginTop: "20px", marginBottom: "20px" }}>
+          Employee Data
+        </h5>
+        <div className="table">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    );
-  }
+    </>
+  );
 }
